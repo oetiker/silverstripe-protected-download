ff0f9958928929235823c19113b03721f37c85c6
Tobias Oetiker <tobi@oetiker.ch>
2016-08-03 15:22:35 +0200

    added PD_Virtual

--- _config.php
+++ _config.php
@@ -2,4 +2,5 @@
 
 define('PROTECTED_DOWNLOAD_DIR',basename(dirname(__FILE__)));
 ShortcodeParser::get()->register('pd_button', array('PD_Button', 'ShortCodeHandler'));
+ShortcodeParser::get()->register('pd_virtual', array('PD_Virtual', 'ShortCodeHandler'));
 DataObject::add_extension('SiteConfig', 'PD_SiteConfig');
---
+++ code/PD_Virtual.php
@@ -0,0 +1,34 @@
+<?php
+/* ************************************************************************   
+   Copyright: 2015 OETIKER+PARTNER AG
+   License:   GPLv3 or later
+   Authors:   Tobi Oetiker <tobi@oetiker.ch>
+   Utf8Check: äöü
+ 
+   Add the [pd_virtual]code[/pd_virtual] short code.
+
+   hides all existing input fields for code and fills them with whatever information is entered here
+   
+   ShortcodeParser::get()->register('pd_virtualn', array('PD_Virtual', 'ShortCodeHandler'));
+ ************************************************************************ */
+
+class PD_Virtual {
+    public static function ShortCodeHandler($arguments,$content = null, $parser = null, $tagName) {
+        return <<<HTML_END
+        <input id="PDV_$content" type="text" width="30/>
+        <script>
+        jQuery(document).ready(function(){
+            jQuery('input[name="$content"]')
+               .parent().parent().hide() /* fieldset */
+               .parent().parent().css({display: 'inline-block'}) /* form */
+               .find('.Actions').css({marginLeft: '0px'}); /* action button */
+        });
+        jQuery('#PDV_$content').pm('change keyup paste',function(){
+             jQuery('input[name="$content"]').val(jQuery(this).val());
+        });
+        </script>
+HTML_END;
+   }
+}
+
+
